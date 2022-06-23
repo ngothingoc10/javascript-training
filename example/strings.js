891dbf799d7baeddc3ea9e46497bbd3f292e5780
ngocngo <ngothingocbk99@gmail.com>
2022-06-23 17:42:38 +0700

    Learn strings in js

---
+++ example/strings.js
@@ -0,0 +1,11 @@
+const a = "Ngoc";
+const b = 'Heloo';
+console.log(b.length);
+console.log(''.length);
+console.log('My name is' + b);
+console.log(`My name is ${b}`);
+const string = `Hey
+this
+string
+is awesome!`
+console.log(string);
